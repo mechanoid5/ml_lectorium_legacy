39b68d3c4ce1fd500d16da51135d78da5abf844c
Evgeny S. Borisov <borisov.e@solarl.ru>
2022-03-11 10:38:47 +0300

    ---

---
+++ examples/11-em/cv-mog.py
@@ -0,0 +1,27 @@
+#!python3
+
+
+# https://pythonprogramming.net/mog-background-reduction-python-opencv-tutorial/
+
+# MOG Background Reduction OpenCV Python Tutorial
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('test.mp4')
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+ 
+    cv2.imshow('fgmask',frame)
+    cv2.imshow('frame',fgmask)
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:  break
+ 
+
+cap.release()
+cv2.destroyAllWindows()
